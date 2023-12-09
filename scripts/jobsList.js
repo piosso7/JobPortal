bf5011678c05dd8cb78e191171ccf387c265b156
Piotr Sokołowski <piosso07@gmail.com>
2023-12-09 18:11:48 +0100

    remove jobPage add popup

--- scripts/jobsList.js
+++ scripts/jobsList.js
@@ -1,3 +1,5 @@
+//display jobs on home page
+
 fetch("../jobs.json")
   .then(function (response) {
     return response.json();
@@ -36,12 +38,92 @@ fetch("../jobs.json")
               </div>
             </div>
           </div>
-          <a href="./pages/jobPage.html"
-            ><button class="jobButton">View Details</button></a
-          >
+          <button id="job${
+            job.id
+          }" class="jobButton" onclick="jobButtonClick(this)">View Details</button>
         </div>
         `;
     }
-
     placeholder.innerHTML = out;
   });
+
+// close popup
+
+const closePopupButton = document.querySelector("#popupClose");
+const jobInfoPopup = document.querySelector("#jobInfoPopup");
+const body = document.body;
+
+function closePopup() {
+  jobInfoPopup.classList.remove("active");
+  jobInfoPopup.classList.add("inactive");
+  body.classList.remove("popupOpen");
+}
+
+// open popup
+
+function openPopup() {
+  jobInfoPopup.classList.add("active");
+  jobInfoPopup.classList.remove("inactive");
+  body.classList.add("popupOpen");
+}
+
+//get clicked button id
+
+const jobButton = document.getElementsByClassName(".jobButton");
+
+let clickedButtonId;
+let clickedButtonNumber;
+
+function jobButtonClick(btn) {
+  clickedButtonId = btn.id;
+  let matches = clickedButtonId.match(/(\d+)/);
+  if (matches) {
+    clickedButtonNumber = matches[0];
+  }
+  getClickedJobInfo();
+  openPopup();
+}
+
+//Get specific page data
+function getClickedJobInfo() {
+  let jobOut = "";
+  fetch("../jobs.json")
+    .then((response) => response.json())
+    .then((data) => {
+      if (Array.isArray(data) && data.length > 1) {
+        const job = data[clickedButtonNumber - 1];
+        console.log(job.id);
+        jobOut = `
+          <div>
+            <div class="jobPageDetailDescription">
+              <div class="details">
+                <p>
+                  <span>Minimum Qualification:</span> ${job.qualification}
+                </p>
+                <p>
+                  <span>Experience Length:</span> ${job.experience}
+                </p>
+                <p>
+                  <span>Location:</span> ${job.location}
+                </p>
+                <p>
+                  <span>Application Deadline:</span> ${job.deadline}
+                </p>
+                <p>
+                  <span>Salary Range:</span> $ ${job.salary}
+                </p>
+              </div>
+              <div class="description">
+              ${job.description}
+              </div>
+            </div>
+          </div>`;
+      }
+
+      document.querySelector("#jobInfoPopupDescription").innerHTML = jobOut;
+      console.log(jobOut);
+    })
+    .catch((error) =>
+      console.error("Błąd podczas pobierania pliku JSON:", error)
+    );
+}
